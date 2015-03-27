2c3cee257fe14ca01ad14e889475c0dfa1986b93
porterjc <porterjc@rose-hulman.edu>
2015-03-27 18:14:15 -0400

    Added minimal code for testfor4 to pass

--- src/PrimeFact.java
+++ src/PrimeFact.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class PrimeFact {
 	public static List<Integer> primeFactorization(int number){
-		return number > 2 ? Arrays.asList(3) : number == 1 ? new ArrayList<Integer>() : Arrays.asList(2);
+		return number > 3 ? Arrays.asList(2, 2) : number == 3 ? Arrays.asList(3) 
+				: number == 1 ? new ArrayList<Integer>() : Arrays.asList(2);
 	}
 }
