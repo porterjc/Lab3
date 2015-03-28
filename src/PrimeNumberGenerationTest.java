6a55f3635361ff75c2df2e422b249c729654c9a2
porterjc <porterjc@rose-hulman.edu>
2015-03-27 21:12:46 -0400

    Added test for input of 4.

--- src/PrimeNumberGenerationTest.java
+++ src/PrimeNumberGenerationTest.java
@@ -31,7 +31,8 @@ public class PrimeNumberGenerationTest {
 		return Arrays.asList(new Object[][] {
 			{1, new ArrayList<Integer>()},
 			{2, new ArrayList<Integer>()},
-			{3, Arrays.asList(2)}
+			{3, Arrays.asList(2)},
+			{4, Arrays.asList(2, 3)}
 		});   
 	   }
 	   
