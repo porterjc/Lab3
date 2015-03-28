71cbc0fd012f60381f2193f57894056c718879e7
porterjc <porterjc@rose-hulman.edu>
2015-03-27 22:51:16 -0400

    wrote minimal code to pass test for 9
    Made it a bit more complex since it would have been more complex to nest
    cases than to write two simple loops

--- src/PrimeNumberGeneration.java
+++ src/PrimeNumberGeneration.java
@@ -7,6 +7,24 @@ public class PrimeNumberGeneration {
 	
 	
 	public static List<Integer> generate(int number){
-		return number > 3 ? Arrays.asList(2, 3): number == 3 ? Arrays.asList(2) : new ArrayList<Integer>();
+		ArrayList<Integer> factors = new ArrayList<Integer>();
+		int num = number;
+		for (int i = 2; i < number; i++) {
+			
+				if(!factors.contains(i) && isPrime(i)){
+	    	  		factors.add(i);
+				
+				num /= i;
+			}
+		}
+		//return number > 3 ? Arrays.asList(2, 3): number == 3 ? Arrays.asList(2) : new ArrayList<Integer>();
+		return factors;
 	}
+	
+	private static boolean isPrime(int number) {
+		for (int i = 2; i < number; i++)
+            if (number % i == 0)
+                return false;
+        return true;
+}
 }
